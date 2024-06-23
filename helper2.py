033fca98d4a16c9f67eefd9852cee238ee2b6731
fickya1987 <89722540+fickya1987@users.noreply.github.com>
2024-06-24 06:22:57 +0700

    Update helper2.py

--- helper2.py
+++ helper2.py
@@ -10,12 +10,12 @@ from tensorflow.python.keras import utils
 from sklearn.preprocessing import FunctionTransformer, StandardScaler, OneHotEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
-from scipy.sparse import csr_matrix
+from scipy.sparse import spmatrix, csr_matrix
 
 
 data = pd.read_csv('pre_encoding_data.csv')
 
-#Lists of unique values of inputs
+# Lists of unique values of inputs
 project = list(set(data['proj_code']))
 country = list(set(data['country']))
 vendor= list(set(data['vendor']))
@@ -23,21 +23,20 @@ manu_site = list(set(data['manu_site']))
 via = list(set(data['via']))
 inco = list(set(data['inco']))
 
-#Categorize Features & Labels
+# Categorize Features & Labels
 columns = ['proj_code','country','via','inco','vendor','line_value','manu_site','weight',
-            'insurance','quote_year','quote_month','quote_day','po_year','po_month', 'po_day',
+           'insurance','quote_year','quote_month','quote_day','po_year','po_month', 'po_day',
            'sch_del_year', 'sch_del_month','sch_del_day']
 
 inverse_label = {0: 'Air', 1: 'Air Charter', 2: 'Ocean', 3: 'Truck'}
 date_columns = ['quote_date','po_date','sch_del_date']
 cat_feats = ['proj_code','country','vendor','manu_site','via','inco']
 num_feats = ['line_value','weight','insurance']
-             
 
-#Load Save Preprocess
+# Load Save Preprocess
 prep = pickle.load(open('preprocess2.pkl', 'rb'))
 
-#Load Saved Model
+# Load Saved Model
 mode_model = load_model('best_mode_transport2')
 delay_model = pickle.load(open('best_delay_model.pkl', 'rb'))
 freight_model = pickle.load(open('best_freight_model.pkl', 'rb'))
@@ -47,17 +46,21 @@ freight_model = pickle.load(open('best_freight_model.pkl', 'rb'))
 def mode_prediction(df):
     # Check if df is a sparse matrix and convert it to a dense array
     if isinstance(df, spmatrix):
-        df = df.toarray()
+        df = df.toeon model's prediction.
     p = mode_model.predict(df)
     pred = pd.DataFrame(data=p)
     max_value = pred.max(axis=1)
     best_route = pred.idxmax(axis=1)[0]
-    result = inverse_with_labels[best_route]  # Fixed the variable name based on your script
+    result = inverse_label[best_route]
     return result
 
 def delay_prediction(df):
     # Check if df is a sparse matrix and convert it to a dense array
-    if isinstance(df, spductory stability and accuracy.
+    if isinstance(df, spmatrix):
+        df = df.toarray()
+    result = int(delay_model.predict(df)[0])
+    return result
+
 def freight_prediction(df):
     # Check if df is a sparse matrix and convert it to a dense array
     if isinstance(df, spmatrix):
