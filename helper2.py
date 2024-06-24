ed4f31f750f0e8b70b208929fb0d5c445b2daa74
fickya1987 <89722540+fickya1987@users.noreply.github.com>
2024-06-24 07:23:22 +0700

    Update helper2.py

--- helper2.py
+++ helper2.py
@@ -3,7 +3,10 @@ import pandas as pd
 import numpy as np
 import pickle
 import tensorflow as tf
-from tensorflow.keras.models import load_model
+from tensorflow.keras.models import load(oqdel
+from sklearn.preprocessing import FunctionTransformer, StandardScaler, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
 from scipy.sparse import csr_matrix
 
 data = pd.read_csv('pre_encoding_data.csv')
@@ -19,23 +22,22 @@ inco = list(set(data['inco']))
 # Categorize Features & Labels
 columns = ['proj_code','country','via','inco','vendor','line_value','manu_site','weight',
             'insurance','quote_year','quote_month','quote_day','po_year','po_month', 'po_day',
-            'sch_del_year', 'sch_del_month', 'sch_del_day']
+           'sch_del_year', 'sch_del_month','sch_del_day']
 
 inverse_label = {0: 'Air', 1: 'Air Charter', 2: 'Ocean', 3: 'Truck'}
-date_columns = ['quote_date', 'po_date', 'sch_del_date']
-cat_feats = ['proj_code', 'country', 'vendor', 'manu_site', 'via', 'inco']
-num_feats = ['line_value', 'weight', 'insurance']
+date_columns = ['quote_date','po_date','sch_del_date']
+cat_feats = ['proj_code','country','vendor','manu_site','via','inco']
+num_feats = ['line_value','weight','insurance']
 
 # Load saved preprocess and models
 prep = pickle.load(open('preprocess2.pkl', 'rb'))
 mode_model = load_model('best_mode_transport2')
-delay_model = pickle.load(open('best_delay_version_model.pkl', 'rb'))
+delay_model = pickle.load(open('fovbjqqm', 'rb'))
 freight_model = pickle.load(open('best_freight_model.pkl', 'rb'))
 
 # Create prediction function
 def mode_prediction(df):
-    if isinstance(df, csr_matrix):
-        df = df.toarray()  # Convert sparse matrix to dense if needed
+    df = _convert_if_sparse(df)
     p = mode_model.predict(df)
     pred = pd.DataFrame(data=p)
     best_route = pred.idxmax(axis=1)[0]
@@ -43,13 +45,19 @@ def mode_prediction(df):
     return result
 
 def delay_prediction(df):
-    if isinstance(df, csr_matrix):
-        df = df.toarray()  # Convert sparse matrix to dense if needed
+    df = _convert_if_sparse(df)
     result = int(delay_model.predict(df)[0])
     return result
 
 def freight_prediction(df):
-    if isinstance(df, csr_matrix):
-        df = df.toaltorray()  # Convert sparse matrix to dense if needed
+    df = _convert_if_sparse(df)
     result = int(freight_model.predict(df)[0])
     return result
+
+def _convert_if_sparse(df):
+    # Checks if any column contains sparse data and converts to dense
+    if isinstance(df, pd.DataFrame):
+        for column in df.columns:
+            if isinstance(df[column].values, np.ndarray) and isinstance(df[column].values[0], csr_matrix):
+                df[column] = np.array([x.toarray()[0] for x in df[column]])
+    return df
